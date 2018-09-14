f27d6fa84c9470133ae2386801469f775e669174
likailun <likailun@bonc.com.cn>
2018-09-14 13:30:15 +0800

    订单详情

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -5,7 +5,8 @@ import LazyComponent from './utils/lazyComponent'
 
 const RouteConfig = [
     {path: '/', component: LazyComponent(import('./routes/home')), exact: true},
-    {path: '/order-detail', component: LazyComponent(import('./routes/orderDetail')), exact: true}
+    {path: '/order-detail', component: LazyComponent(import('./routes/orderDetail')), exact: true},
+    {path: '/submit-order', component: LazyComponent(import('./routes/submitOrder')), exact: true},
 ]
 
 export default RouteConfig
