2f6ea7fe82b223ec7d8252a2c230d782c5d777c0
likailun <likailun@bonc.com.cn>
2018-09-28 15:31:15 +0800

    支付功能测试

--- src/routes/message/success.tsx
+++ src/routes/message/success.tsx
@@ -1,15 +1,23 @@
 import { Component } from 'react'
 import { MessagePage } from '../../components'
 import Block from 'fs-flex'
+import {Button} from 'antd-mobile'
 
 class SuccessPage extends Component{
     componentDidMount(){
         document.title='支付成功'
     }
+    payTest=()=>{
+        //https://wxpay.wxutil.com/mch/pay/h5.v2.php
+        window.location.href='https://wxpay.wxutil.com/mch/pay/h5.v2.php'
+    }
     render(){
         const {match:{params:{orderId}}}  =this.props
         return <Block vf>
             <MessagePage status='success' message='支付成功' linkText='查看订单' linkUrl={`/order-complete/${orderId}`} />
+            <Block>
+                <Button onClick={this.payTest}>支付测试</Button>
+            </Block>
         </Block>
     }
 }
