da5c58e169af938c0f37a19aa3103807dd66b927
zouyonghong <641261604@qq.com>
2018-09-29 09:27:30 +0800

    no message

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -8,19 +8,19 @@ import {homePage, orderDetailPage, myOrderPage, orderSurePage, accountPage, addr
 const RouteConfig = [
     {path: '/home/:sessionId/:memId', component: homePage, exact: true},
     {path: '/order-detail/:pid/:sessionId/:memId', component: orderDetailPage, exact: true},
-    {path: '/my-order', component: myOrderPage, exact: false},
+    {path: '/my-order/:sessionId/:memId', component: myOrderPage, exact: false},
     {path: '/order-sure/:shoppingcardId/:sessionId/:memId', component: orderSurePage, exact: true},
     {path: '/account/:sessionId/:memId', component: accountPage, exact: false},
-    {path: '/address-mgr', component: addressMgrPage, exact: true},
-    {path: '/add-address', component: addAddressPage, exact: true},
-    {path: '/update-address', component: updateAddressPage, exact: true},
-    {path: '/order-info/:orderId', component: orderInfoPage, exact: true},
+    {path: '/address-mgr/:sessionId/:memId', component: addressMgrPage, exact: true},
+    {path: '/add-address/:sessionId/:memId', component: addAddressPage, exact: true},
+    {path: '/update-address/:sessionId/:memId', component: updateAddressPage, exact: true},
+    {path: '/order-info/:orderId/:sessionId/:memId', component: orderInfoPage, exact: true},
     {path: '/product/:typeId/:sessionId/:memId', component: productPage, exact: true},
     {path: '/search/:parentType/:name/:sessionId/:memId', component: searchPage, exact: true},
     {path: '/order-complete/:orderId/:sessionId/:memId', component: orderCompletePage, exact: true},
     {path: '/cart/:sessionId/:memId', component: cartPage, exact: true},
     {path: '/success/:orderId/:sessionId/:memId', component: successPage, exact: true},
-    {path: '/fail/:sessionId/:memId', component: failPage, exact: true}
+    {path: '/fail', component: failPage, exact: true}
 ]
 
 export default RouteConfig
