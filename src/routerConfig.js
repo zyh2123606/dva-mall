b97864dfedafe118ace097004110cfad2ab65acb
zouyonghong <zouyonghong@bonc.com.cn>
2018-10-28 15:19:55 +0800

    修改路由配置

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -1,16 +1,14 @@
 /**
  * 路由配置
  */
-import {homePage, orderDetailPage, myOrderPage, orderSurePage, accountPage, addressMgrPage, 
-    addAddressPage,updateAddressPage, orderInfoPage, orderCompletePage, productPage, searchPage, 
-    cartPage, successPage, failPage,deptSelect} from './lazyRoutes'
+import {orderDetailPage, myOrderPage, orderSurePage, addressMgrPage, 
+    addAddressPage,updateAddressPage, orderInfoPage, orderCompletePage, productPage, searchPage, successPage, failPage, deptSelect, defaultPage} from './lazyRoutes'
     
 const RouteConfig = [
-    {path: '/home', component: homePage, exact: true},
+    {path: '/', component: defaultPage},
     {path: '/order-detail/:pid/:sessionId/:memId', component: orderDetailPage, exact: true},
     {path: '/my-order', component: myOrderPage, exact: false},
     {path: '/order-sure', component: orderSurePage, exact: true},
-    {path: '/account/:sessionId/:memId', component: accountPage, exact: true},
     {path: '/address-mgr/:sessionId/:memId', component: addressMgrPage, exact: true},
     {path: '/add-address/:sessionId/:memId', component: addAddressPage, exact: true},
     {path: '/update-address/:sessionId/:memId', component: updateAddressPage, exact: true},
@@ -18,7 +16,6 @@ const RouteConfig = [
     {path: '/product/:typeId/:sessionId/:memId', component: productPage, exact: true},
     {path: '/search/:parentType/:sessionId/:memId', component: searchPage, exact: true},
     {path: '/order-complete/:orderId/:sessionId/:memId', component: orderCompletePage, exact: true},
-    {path: '/cart/:sessionId/:memId', component: cartPage, exact: true},
     {path: '/success/:orderId/:sessionId/:memId', component: successPage, exact: true},
     {path: '/fail', component: failPage, exact: true},
     {path: '/dept-select/:sessionId/:memId', component: deptSelect, exact: true}
